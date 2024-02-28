351343ef3a1c490bb8c72821da39c81946cbc5c9
TamasGalambos <tamas.galambos05@htl-salzburg.ac.at>
2024-02-28 10:15:19 +0100

    Mermaid Classes created

---
+++ Assets/MyGame/Scripts/EnemyController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private float chaseSpeed;
+    private Transform target; 
+
+    void Update()
+    {
+        
+    }
+}
